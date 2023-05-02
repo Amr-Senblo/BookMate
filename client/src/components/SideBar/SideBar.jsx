dc94d9794a7e9c7250bc780968a5c2e78f530ed8
Mohamed Harby <mohamed.harby.ce@gmail.com>
2023-05-02 17:57:41 +0300

    feat: Login/Register-popup-added

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./SideBar.scss";
 import MenuIcon from "../../assets/svg/menu.svg";
+import Popup from "../popup/Popup";
 
 const SideBar = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
@@ -9,24 +10,48 @@ const SideBar = () => {
   const handleToggle = () => {
     setIsCollapsed(!isCollapsed);
   };
+  const [user, setUser] = useState(null);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const openPopup = () => {
+    setIsPopupOpen(true);
+  };
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
 
   return (
     <div className={`sidebar ${isCollapsed ? "collapsed" : ""}`}>
       <div className="user">
-        <button className="user-logout" alt="Logout">
-          <span className="logout-icon"></span>
-        </button>
-        <div className="user-info">
-          <img
-            className="user-image"
-            src="https://picsum.photos/200"
-            alt="user"
-          />
-          <div className="user-details">
-            <span className="user-name">Username</span>
-            <span className="user-stars">100 stars</span>
-          </div>
-        </div>
+        {user ? (
+          <>
+            <button className="user-logout" alt="Logout">
+              <span className="logout-icon"></span>
+            </button>
+            <div className="user-info">
+              <img
+                className="user-image"
+                src="https://picsum.photos/200"
+                alt="user"
+              />
+              <div className="user-details">
+                <span className="user-name">Username</span>
+                <span className="user-stars">100 stars</span>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <button
+              className="user-login-register"
+              alt="Login/Register"
+              onClick={openPopup}
+            >
+              <span className="login-register-text">Login/Register</span>
+              <span className="logout-icon"></span>
+            </button>
+          </>
+        )}
       </div>
 
       <nav>
@@ -84,6 +109,7 @@ const SideBar = () => {
           </li>
         </ul>
       </nav>
+      {isPopupOpen && <Popup onClose={closePopup} />}
     </div>
   );
 };
--- client/src/components/popup/Popup.jsx
+++ client/src/components/popup/Popup.jsx
@@ -1,8 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useRef, useState } from "react";
-import "./Popup.css";
+import "./Popup.scss";
 import useClickOutside from "../../custom/useClickOutside";
-import CloseIcon from "../../assets/svg/close_icon.svg";
 
 const Popup = ({ onClose }) => {
   const [showLogin, setShowLogin] = useState(true);
@@ -36,8 +35,9 @@ const Popup = ({ onClose }) => {
         ref={popupRef}
         style={{
           opacity: isVisible ? 1 : 0,
-          transform: isVisible ? "translateY(0)" : "translateY(-50%)",
-          transition: "opacity 0.3s ease, transform 0.3s ease",
+          transform: isVisible ? "scale(1)" : "scale(0.8)",
+          transition:
+            "opacity 0.4s ease-in-out, transform 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94)",
         }}
       >
         <div className="popup__content__header">
@@ -46,17 +46,18 @@ const Popup = ({ onClose }) => {
             className="popup__content__header__close"
             onClick={closePopup}
           >
-            <img src={CloseIcon} alt="close" width={18} height={18} />
+            <span className="close-icon" />
+            {/* <img src={CloseIcon} alt="close" width={18} height={18} /> */}
           </button>
         </div>
         {showLogin ? (
           <form className="popup__content__form">
             <label>
-              Email
+              <span className="label-name">Email</span>
               <input type="email" name="email" required />
             </label>
             <label>
-              Password
+              <span className="label-name">Password</span>
               <input type="password" name="password" required />
             </label>
             <button type="submit">Login</button>
@@ -64,21 +65,21 @@ const Popup = ({ onClose }) => {
         ) : (
           <form className="popup__content__form">
             <label>
-              Email
+              <span className="label-name">Email</span>
               <input type="email" name="email" required />
             </label>
             <label>
-              Password
+              <span className="label-name">Password</span>
               <input type="password" name="password" required />
             </label>
             <label>
-              Confirm password
+              <span className="label-name">Confirm password</span>
               <input type="password" name="confirm-password" required />
             </label>
             <button type="submit">Register</button>
           </form>
         )}
-        <p>
+        <p className="toggle-text">
           {showLogin
             ? "Don't have an account yet?"
             : "Already have an account?"}{" "}
