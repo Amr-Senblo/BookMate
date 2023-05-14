6143351418ed2c5f6f5b52f2731cf1a61dc9bd77
Mohamed Harby <mohamed.harby.ce@gmail.com>
2023-05-14 18:52:16 +0300

    feat: card container fixed

--- client/src/components/BookCard/BookCard.jsx
+++ client/src/components/BookCard/BookCard.jsx
@@ -4,18 +4,16 @@ import "./BookCard.scss";
 
 // import CheckedHeart from "../../assets/svg/checked-heart.svg";
 // import UnCheckedHeart from "../../assets/svg/unchecked-heart.svg";
-import StarIcon from "../../assets/svg/star.svg";
 import { Link } from "react-router-dom";
 
 const BookCard = ({ book }) => {
   return (
-    <Link to={`/book/id=${book.id}`} style={{ textDecoration: "none" }}>
+    <Link to={`/book/id=${book._id}`} style={{ textDecoration: "none" }}>
       <div className="card">
         <div className="card-image">
-          <img src={book.cover} alt={book.title} />
+          <img src={"https://picsum.photos/200"} alt={book.title} />
           <div className="rate">
-            <img src={StarIcon} alt="Star Icon" className="star-icon" />
-            <p className="rate-number">{` ${book.stars}`}</p>
+            <p className="rate-number">{` ${book.rating}`}</p>
           </div>
         </div>
         {/* <div className="card-header"> */}
@@ -29,11 +27,11 @@ const BookCard = ({ book }) => {
 
 BookCard.propTypes = {
   book: PropTypes.shape({
-    id: PropTypes.number.isRequired,
+    _id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
-    cover: PropTypes.string.isRequired,
-    stars: PropTypes.number.isRequired,
+    // imageUrl: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
   }).isRequired,
 };
 
--- client/src/components/cardContainer/CardContainer.jsx
+++ client/src/components/cardContainer/CardContainer.jsx
@@ -2,11 +2,29 @@
 import "./CardContainer.css";
 import BookCard from "../BookCard/BookCard";
 import SectionHeader from "../SectionHeader/SectionHeader";
+import { useEffect, useState } from "react";
 // import WhiteButton from "../WhiteButton/WhiteButton";
 // import { Link } from "react-router-dom";
-import books from "../../data/books";
+// import books from "../../data/books";
+import axios from "axios";
 
 const CardContainer = () => {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    try {
+      axios
+        .get(`http://localhost:6969/api/v1/books`, {
+          withCredentials: true,
+        })
+        .then((res) => {
+          console.log(res);
+          setBooks(res.data.books);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
     <>
       <SectionHeader title="Explore" />
